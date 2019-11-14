e9730ce6ea85a79f6a0d59fd5dab64334ccc2ec8
coyotew1 <55400703+coyotew1@users.noreply.github.com>
2019-11-14 13:55:12 -0700

    1.1.2

---
+++ Rectangle.java
@@ -0,0 +1,18 @@
+/**
+ * Rectangle
+ */
+public class Rectangle {
+    private double length;
+    private double width;
+
+    public Rectangle ()
+    {
+        this.length = 0;
+        this.width = 0;
+    }
+
+    public Rectangle (length, width)
+    {
+
+    }
+}
