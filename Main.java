57966a7b748147545330129ec009c0c3b0945b3f
coyotew1 <55400703+coyotew1@users.noreply.github.com>
2019-11-16 09:11:10 -0700

    1.2.1

--- Main.java
+++ Main.java
@@ -4,6 +4,30 @@
 public class Main {
 
     public static void main(String[] args) {
-        
+        Rectangle[] rectangles = new Rectangle[] {
+            new Rectangle(2,5,"Rectangle 1"),
+            new Rectangle(5, 6, "Rectangle 2"),
+            new Rectangle(2,7,"Rectangle 3"),  
+        };
+
+
+        //displays information regarding the rectangle objects initialized in the array above.
+        for (int i = 0; i<rectangles.length;i++)
+        {
+            System.out.println(rectangles[i]);
+        }
+
+        //displays the area of all rectangles in the above array
+        for (int i=0;i<rectangles.length;i++)
+        {
+            System.out.println("The area of " + rectangles[i].getName() + " is " + rectangles[i].area() + " inches\n");
+        }
+
+        //displays the perimeter of all rectangles in the above array
+        for (int i=0;i<rectangles.length;i++)
+        {
+            System.out.println("The perimeter of " + rectangles[i].getName() + " is " + rectangles[i].perimeter() + " inches\n");
+        }
+
     }
 }
--- Rectangle.java
+++ Rectangle.java
@@ -4,15 +4,79 @@
 public class Rectangle {
     private double length;
     private double width;
+    private String name;
 
-    public Rectangle ()
+    public Rectangle(double length, double width, String name)
+    {
+        this.length = length;
+        this.width = width;
+        this.name = name;
+    }
+
+    public Rectangle(double length, double width)
+    {
+        this.length = length;
+        this.width = width;
+        this.name = "";
+    }
+
+    public Rectangle()
     {
         this.length = 0;
         this.width = 0;
+        this.name = "";
     }
 
-    public Rectangle (length, width)
+    
+    //mutators
+    public void setLength(double length)
     {
-
+        this.length = length;
     }
+
+    public void setWidth(double width)
+    {
+        this.width = width;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    //accessors
+    public Double getLength()
+    {
+        return this.length;
+    }
+
+    public Double getWidth()
+    {
+        return this.width;
+    }
+
+    public String getName()
+    {
+        return this.name;
+    }
+
+
+    //additional methods specific to this object
+
+    public Double area()
+    {
+        return this.width*this.length;
+    }
+
+    public Double perimeter()
+    {
+        double a = this.length*2;
+        double b = this.width*2;
+        return a+b;
+    }
+    @Override
+    public String toString() {
+        return "Name: "  + this.name + "\n" + "Length: " + this.length + "\nwidth: " + this.width;
+    }
+    
 }
